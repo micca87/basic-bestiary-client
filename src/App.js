e44ad760258b2fcfa366f017871612b960c454c6
micca87 <micca87@gmail.com>
2021-02-23 22:12:13 +0100

    Add sign in/up components and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
+import { Link, BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <BrowserRouter>
+        <div className="App">
+            <ul>
+              <li>
+                <Link to="/">Zaloguj sie</Link>
+              </li>
+              <li>
+                <Link to="/register">Zarejestruj się</Link>
+              </li>
+            </ul>
+
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/register">
+            <SignUp />
+          </Route>
+        </Switch>
+        <Link to="/guest" >Wejdź bez logowania</Link>
+        </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SignIn() {
+    return (
+        <div>
+            <form>
+                <label for="name">Enter your email:</label>
+                <input type="email" name="email" required></input>
+                <label for="password">Enter password:</label>
+                <input type="password" name="password" required></input>
+            </form>
+        </div>
+    )
+}
+
+export default SignIn;
